f636feb7b296f55084b5db40cf7b24d64c5c418c
peterhoth <hoth.peter@gmail.com>
2025-10-07 15:40:06 +0800

    update

--- setup-app.sh
+++ setup-app.sh
@@ -88,13 +88,13 @@ else
 
     OCI_IMAGE="docker.io/hwkcld/$IMAGE"
 
-    podman pull docker.io/library/busybox
+    podman pull --pull=newer docker.io/library/busybox
     if [[ $? -ne 0 ]]; then
         echo "Failed downloading busybox."
         exit 1
     fi
 
-    podman pull ${OCI_IMAGE}
+    podman pull --pull=newer ${OCI_IMAGE}
     if [[ $? -ne 0 ]]; then
         echo "Failed downloading ${OCI_IMAGE}."
         exit 1
